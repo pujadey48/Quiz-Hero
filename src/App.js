30ee0ae89d9ba493c972201cf8b65b7c8e16613a
Puja Dey <pujadey48@gmail.com>
2022-10-11 03:43:09 +0600

    creater router and some components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const router = createBrowserRouter([
+
+  ])
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
